5a94f5c1a23f40ec18193eda87454c1d04aea126
Matheus Rocha <matheusrochaalmeida7@gmail.com>
2025-10-18 13:38:22 -0300

    pagina inicial pronta

---
+++ assets/scripts/card-hover-animation.js
@@ -0,0 +1,25 @@
+
+
+
+function handleMouseEnter(){
+    this.classList.add('s-card--hovered');
+
+    document.body.id = `${this.id}-hovered`;
+}
+
+function handleMouseLeave(){
+    this.classList.remove('s-card--hovered')
+    document.body.id = '';
+}
+
+
+function addEventListenersToCards(){
+    const cardElements = Array.from(document.getElementsByClassName('s-card'));
+    cardElements.forEach(card => {
+        card.addEventListener('mouseenter', handleMouseEnter);
+        card.addEventListener('mouseleave', handleMouseLeave);
+    });
+}
+
+
+document.addEventListener("DOMContentLoaded", addEventListenersToCards);
